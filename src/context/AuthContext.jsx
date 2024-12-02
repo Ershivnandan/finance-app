cd32c730bf0a649b24618d97b50a925f30858fe1
ErShivnandan <ec1912@global.org.in>
2024-12-02 18:27:09 +0530

    auth context updated

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+
 import { createContext, useContext, useState, useEffect } from "react";
 import { auth } from "../utils/firebase.config"; 
 import { onAuthStateChanged, signOut } from "firebase/auth";
